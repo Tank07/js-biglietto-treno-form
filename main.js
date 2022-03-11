aff1d5ce6e5cbb845818e39e298d2157fe075cc4
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-11 01:11:24 +0100

    Genera/Annulla bottoni funzionanti

--- main.js
+++ main.js
@@ -1,60 +1,54 @@
-// let chilometriUtente = Number(prompt ("Quanti chilometri dovrà percorrere ?"));
 
-// let etaUtente = Number(prompt ("Quanti anni ha ?"));
 
-// console.log(chilometriUtente + etaUtente);
+  function myGenera() {
 
-// let prezzoBase = chilometriUtente * 0.21;
-
-// console.log(prezzoBase);
-
-// let prezzoUnder = prezzoBase - (prezzoBase/100 * 20); 
-
-// let prezzoOver = prezzoBase - (prezzoBase/100 * 40);
-
-// if ( etaUtente < 18 ) {
-//     //istruzioni se condizione è vera
-//     console.log(prezzoUnder.toFixed(2))
-//     document.getElementById("contenitore_prezzo").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoUnder}€</h2>`;
-//   } else if ( etaUtente > 65) {
-//     //istruzioni se condizione 2 è vera
-//     console.log(prezzoOver.toFixed(2))
-//     document.getElementById("contenitore_prezzo").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoOver}€</h2>`;
-//   } else {
-//     //istruzioni nessuna condizione precedente è vera
-//     console.log(prezzoBase.toFixed(2))
-//     document.getElementById("contenitore_prezzo").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoBase}€</h2>`;
-//   }
-
-  function myFunction() {
+    qualcosa.classList.remove("d-none");
 
     let nomeUtente = document.getElementById("nome").value
 
-    let km = document.getElementById("kmNumber").value;
+    let chilometriUtente = document.getElementById("kmNumber").value;
     
     let etaUtente = document.getElementById("eta").value;
 
-    document.getElementById("demo").innerHTML = nomeUtente + km + etaUtente;
+    let prezzoBase = chilometriUtente * 0.21;
+
+    let prezzoUnder = prezzoBase - (prezzoBase * 0.2); 
+   
+    let prezzoOver = prezzoBase - (prezzoBase * 0.4);
+   
+    // if ( etaUtente == "minorenne" ) {
+    //     //istruzioni se condizione è vera
+    //     console.log(prezzoUnder.toFixed(2))
+    //     document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoUnder}€</h2>`;
+    //   } else if ( etaUtente == "over" ) {
+    //     //istruzioni se condizione 2 è vera
+    //     console.log(prezzoOver.toFixed(2))
+    //     document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoOver}€</h2>`;
+    //   } else {
+    //     //istruzioni nessuna condizione precedente è vera
+    //    console.log(prezzoBase.toFixed(2))
+    //    document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoBase}€</h2>`;
+    //  }
+
+    console.log(nomeUtente + chilometriUtente + etaUtente)
+
+    document.getElementById("nome_passeggero").innerHTML = nomeUtente;
+
+    document.getElementById("carrozza").innerHTML = km;
   }
 
 
 
 //al click sul bottone compare un testo
 //selezionare l'elemento del DOM
-let bottone = document.getElementById("bottoneShowHide");
-let nota = document.getElementById("notaSelect");
+let annulla = document.getElementById("bottoneHide");
+let qualcosa = document.getElementById("annullatodo");
 
 //creare l'evento al click
-bottone.addEventListener("click",
+annulla.addEventListener("click",
 
   //istruzioni che succedono dopo il click sull'elemento
   function() {
-       nota.classList.add("d-none");
-       if( nota.classList.contains("d-none") ){
-         nota.classList.remove("d-none");
-       } else {
-         nota.classList.add("d-none");
-       }
-      nota.classList.toggle("d-none");
+    qualcosa.classList.add("d-none");
   }
 );
