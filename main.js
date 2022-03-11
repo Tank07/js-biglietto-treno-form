67e7841d27faac50f895ef568870040631d5b03e
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-11 01:30:13 +0100

    Esteticamente orrendo ma funziona

--- main.js
+++ main.js
@@ -13,28 +13,33 @@
     let prezzoBase = chilometriUtente * 0.21;
 
     let prezzoUnder = prezzoBase - (prezzoBase * 0.2); 
+
+    console.log(prezzoUnder)
    
     let prezzoOver = prezzoBase - (prezzoBase * 0.4);
    
-    // if ( etaUtente == "minorenne" ) {
-    //     //istruzioni se condizione è vera
-    //     console.log(prezzoUnder.toFixed(2))
-    //     document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoUnder}€</h2>`;
-    //   } else if ( etaUtente == "over" ) {
-    //     //istruzioni se condizione 2 è vera
-    //     console.log(prezzoOver.toFixed(2))
-    //     document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoOver}€</h2>`;
-    //   } else {
-    //     //istruzioni nessuna condizione precedente è vera
-    //    console.log(prezzoBase.toFixed(2))
-    //    document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoBase}€</h2>`;
-    //  }
+     if ( etaUtente == "minorenne" ) {
+         console.log(prezzoUnder.toFixed(2))
+         document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoUnder}€</h2>`;
+         document.getElementById("tipo_biglietto").innerHTML = `<h2>Biglietto Sconto Minorenne</h2>`;
+       } else if ( etaUtente == "over" ) {
+         console.log(prezzoOver.toFixed(2))
+         document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoOver}€</h2>`;
+         document.getElementById("tipo_biglietto").innerHTML = `<h2>Biglietto Sconto Over 65</h2>`;
+       } else {
+        console.log(prezzoBase.toFixed(2))
+        document.getElementById("costo_finale").innerHTML = `<h2>Il prezzo del suo biglietto è  ${prezzoBase}€</h2>`;
+        document.getElementById("tipo_biglietto").innerHTML = `<h2>Biglietto Standard</h2>`;
+      }
 
     console.log(nomeUtente + chilometriUtente + etaUtente)
 
-    document.getElementById("nome_passeggero").innerHTML = nomeUtente;
+    document.getElementById("nome_passeggero").innerHTML =  `<h2> ${nomeUtente}</h2>`;
 
-    document.getElementById("carrozza").innerHTML = km;
+    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 9) + 1;
+
+    document.getElementById("codice").innerHTML = Math.floor(Math.random() * 9000) + 1111;
+
   }
 
 
